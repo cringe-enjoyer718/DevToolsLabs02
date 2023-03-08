1741a18f8573be2f8e0bd9f353f0923fc27ea51f
Веретенникова Анастасия <animeshnik718@gmail.com>
2023-03-08 14:50:03 +0400

    Check the package

---
+++ NugetCheck/NugetCheck/Program.cs
@@ -0,0 +1,13 @@
+﻿using PowerCollections;
+
+PowerCollections.Stack<int> stack = new (10);
+
+for(int i = 0; i < 10; i++)
+{
+    stack.Push(i);
+}
+
+foreach(int i in stack)
+{
+    Console.WriteLine(i);
+}
